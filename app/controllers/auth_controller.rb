271297daf56f2b5288273d378deca9e8a5eaba42
Raghuveera Betina <rbetina@gmail.com>
2012-06-15 09:31:40 -0500

    Got the access token.

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,5 +1,20 @@
+require 'open-uri'
+
 class AuthController < ApplicationController
   def facebook
-    @code = params[:code]
+    code = params[:code]
+    
+    uri =  "https://graph.facebook.com/oauth/access_token?client_id=249187265194708&redirect_uri=http://localhost:3000/auth/facebook&client_secret=9a7277bdb4b1ff0ad232eadd806e5011&code=#{@code}"
+    response = open(uri).read
+    
+    access_token = response.split('&').first.split('=').last
+    
+    user = User.find(session[:user_id])
+    
+    user.facebook_access_token = access_token
+    
+    user.save
+    
+    redirect_to user
   end
 end
